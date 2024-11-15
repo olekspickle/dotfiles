b58477b8b96e69bcbc7b90c136685b3854780451
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    fix path in rust-analyzer installation; simplify openvpn install

--- rust-analyzer.sh
+++ rust-analyzer.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail; shopt -s failglob # safe mode 
 
- mkdir -p ~/.local/bin
- curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
- chmod +x ~/.local/bin/rust-analyzer
+mkdir -p ~/.local/bin
+curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer
+echo "export PATH=$PATH:/home/pickle/.local/bin" >> ~/.zshrc
 
--- vpn.sh
+++ vpn.sh
@@ -5,15 +5,20 @@ echo $version
 #sudo apt install libssl-dev liblzo2-dev libpam0g-dev wget
 
 #wget -r https://openvpn.net/index.php/download/community-downloads.html
-wget https://swupdate.openvpn.org/community/releases/openvpn-$version.tar.gz
+#wget https://swupdate.openvpn.org/community/releases/openvpn-$version.tar.gz
+#tar -zxf openvpn-$version.tar.gz
+#cd openvpn-$version
+#./configure
+#make 
+#make install
+sudo apt install openvpn easy-rsa
 
-tar -zxf openvpn-$version.tar.gz
+# create a new directory on the OpenVPN Server
+mkdir ~/easy-rsa
+# create a symlink from the easyrsa script that the package installed into the ~/easy-rsa directory
+ln -s /usr/share/easy-rsa/* ~/easy-rsa/
 
-cd openvpn-$version
-
-./configure
-
-make 
-
-make install
+# ensure the directory’s owner is your non-root sudo user and restrict access to that user
+sudo chown $USER ~/easy-rsa
+chmod 700 ~/easy-rsa
 
