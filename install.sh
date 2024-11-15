487d2921623253e38635497fce65a1eb9acf9c55
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:51:46 +0100

    add font install

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 sudo apt-get install python3 build-essential \
     && ./install_images_related.sh \
-    && ./install_psql.sh
+    && ./install_psql.sh \
+    && ./install_fonts.sh \ 
+    && ./install_node.sh \
+    && ./install_rust.sh \
+    && sudo snap install obs-studio
