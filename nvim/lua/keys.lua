7aa5ed64d9058ef6ef6580aefda74199522d3c03
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    fix nvim folder; update alacritty config

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -6,6 +6,8 @@ local map = vim.api.nvim_set_keymap
 -- command: the command you want the keypresses to execute.
 -- options: an optional Lua table of options to configure (e.g., silent or noremap).
 
+-- alacritty + zellij + nvim are not perfect in unicode handling not even sure this helps
+-- it is a failed attempt to get Ctrl + Tab switch buffers working
 if vim.env.TERM == 'alacritty' then
   vim.cmd([[autocmd UIEnter * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[>1u") | endif]])
   vim.cmd([[autocmd UILeave * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[<1u") | endif]])
@@ -22,7 +24,7 @@ map('n', 't', [[:TagbarToggle<cr>]], {})
 map('n', 'ff', [[:Telescope find_files<cr>]], {})
 
 -- Rust compile
--- map('n', 'c-r', [[:FloatermNew --height=0.6 --width=0.4 --wintype=float --name=floaterm1 --position=topleft --autoclose=2 ranger --cmd="cargo check"]], {})
+map('n', 'c-r', [[:FloatermNew --height=0.6 --width=0.4 --wintype=float --name=floaterm1 --position=topleft --autoclose=2 ranger --cmd="echo lol"]], {})
 
 -- Buffer switch
 map('n', '<c-i>', [[:bn<cr>]], {})
