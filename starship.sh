cf34922705deeac2a462b8601c3e342f8e78bac8
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    add vagrant install

--- starship.sh
+++ starship.sh
@@ -5,7 +5,9 @@ set -euo pipefail; shopt -s failglob # safe mode
 echo "Installing starship..."
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -y
 
-echo `eval "$(starship init zsh)"` >> ~/.zshrc
-
-
+echo `
+eval "$(starship init zsh)"
+# starship char leftover override
+export LC_CTYPE=en_US.UTF-8
+` >> ~/.zshrc
 
---
+++ vagrant.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./hashicorp-keyring.sh
+sudo apt update && sudo apt install vagrant
