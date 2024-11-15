3ab6a414d10ee8bc0222b6806235c5a55eb1c182
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:51:46 +0100

    Add lld linker for rust

--- install_rust.sh
+++ install_rust.sh
@@ -3,5 +3,18 @@
 set -euo pipefail; shopt -s failglob # safe mode 
 
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh \
-    && source $HOME/.cargo/env
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
+source $HOME/.cargo/env
+
+sudo apt install -y lld 
+
+CONFIG='
+[build]
+rustflags = [
+  "-C", "link-arg=-fuse-ld=lld",
+]
+'
+
+echo $CONFIG >> ~/.cargo/config
+
+
