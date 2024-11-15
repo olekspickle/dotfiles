748f4dc3ec374d0042f7ffc62dd3e0741cc0ccd2
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    add yt downloader

---
+++ youtube.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# download and install youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
