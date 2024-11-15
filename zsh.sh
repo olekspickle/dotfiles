e5f00bfe7475cb8b6d07f4d65b85f5aa570467fb
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    add zsh autosuggestion to install

--- zsh.sh
+++ zsh.sh
@@ -26,3 +26,12 @@ echo "ZSH_THEME=powerlevel9k/powerlevel9k" >> ~/.zshrc
 echo "POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vcs dir rbenv)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(root_indicator background_jobs status load)" >> ~/.zshrc
 
+echo Install zsh zsh-autosuggestions...
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Manually add the plugin to the list of plugins for Oh My Zsh to load (inside ~/.zshrc):
+#
+# plugins=(
+#     # other plugins...
+#     zsh-autosuggestions
+# )
