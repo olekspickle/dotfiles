bdaafe8f15a81dc9b9c269c30d56bd2fc8351ad5
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    update alacritty config

--- alacritty.sh
+++ alacritty.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 git clone https://github.com/alacritty/alacritty
 
 cd alacritty
@@ -11,25 +13,32 @@ cargo build --release
 # terminfo
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
 
-# change default terminal
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /home/pickle/.cargo/bin/alacritty 50
-
 # To check this should point to alacritty auto
 # sudo update-alternatives --config x-terminal-emulator
 
+path="/usr/local/bin"
+
 # Desktop support
-sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp target/release/alacritty $path
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
+
+# change default terminal
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator  $path 50
+sudo chown root:root $path
+sudo chmod --reference=/usr/bin/ls $path
 
 # shell completions
 mkdir -p ${ZDOTDIR:-~}/.zsh_functions
 echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
 cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
 
+cd ..
+
 # copy config file
 cp alacritty.yml ~/.config/alacritty/alacritty.yml
 
-cd ..
 rm -rf alacritty
+
+echo "Done! Alacritty is your terminal emulator now. It's good to have you back! A."
