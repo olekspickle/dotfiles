3219f374d5866b2bca86596c21a574c580cb83bb
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:52:45 +0100

    add starship eval for zsh

--- starship.sh
+++ starship.sh
@@ -5,7 +5,7 @@ set -euo pipefail; shopt -s failglob # safe mode
 echo "Installing starship..."
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -y
 
-echo `eval "$(starship init zsh)"`
+echo `eval "$(starship init zsh)"` >> ~/.zshrc
 
 
 
