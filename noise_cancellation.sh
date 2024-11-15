ffb36b5e59d6a3a1cc6f5439ee739cf6a2894c05
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:52:45 +0100

    add reference

--- noise_cancellation.sh
+++ noise_cancellation.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail; shopt -s failglob # safe mode 
 
+# thx to this reddit post:
+# https://www.reddit.com/r/linuxmasterrace/comments/g7mikg/rtx_voice_on_linux/
+
 echo "Installing noise-cancellation plugin..."
 ./filter_voice.sh
+
+echo "To install frontend to noise cancellation go to https://github.com/josh-richardson/cadmus/"
