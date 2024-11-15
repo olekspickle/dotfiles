f52b83e3a43b67aaf203b1a4987a442e6291e182
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    update rust scripts

--- rust-analyzer.sh
+++ rust-analyzer.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail; shopt -s failglob # safe mode 
 
-mkdir -p ~/.local/bin
-curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
-chmod +x ~/.local/bin/rust-analyzer
-echo "export PATH=$PATH:/home/pickle/.local/bin" >> ~/.zshrc
+RA_PATH="/home/pickle/.local/bin"
+
+mkdir -p $RA_PATH
+curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ${RA_PATH}/rust-analyzer
+
+chmod +x ${RA_PATH}/rust-analyzer
+echo "export PATH=$PATH:${RA_PATH}" >> ~/.zshrc
 
