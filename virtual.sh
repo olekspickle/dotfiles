323a94e95aca5a0e7afcb3aa6afe7083463e7797
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    perfect virtualbox script

--- virtual.sh
+++ virtual.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-curl -sSO https://download.virtualbox.org/virtualbox/6.1.34/virtualbox-6.1_6.1.34-150636.1~Ubuntu~eoan_amd64.deb vurtual.deb
+set -e
 
-sudo apt install ./virtual.deb
+v=$(curl -sS https://download.virtualbox.org/virtualbox/LATEST.TXT)
+echo Version: $v
+codename=$(lsb_release -c | awk '{print $2}')
 
+url=$(curl -sS https://download.virtualbox.org/virtualbox/$v | rg 'Ubuntu' | rg $codename | awk '{split($0, arr, "[\"\"]"); print arr[2]}')
+ref="https://download.virtualbox.org/virtualbox/$v/$url"
+
+echo Downloading virtualbox deb package for $codename...
+echo $ref
+
+#curl -sS $ref -o virtual.deb
+
+#sudo apt install ./virtual.deb
+
+#wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+
+#rm virtual.deb
