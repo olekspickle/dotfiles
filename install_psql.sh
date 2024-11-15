8848820dad6b4c0cc7789666deafde52a6b8912b
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:51:46 +0100

    update psql script

--- install_psql.sh
+++ install_psql.sh
@@ -1,3 +1,9 @@
 # psql
-sudo apt-get install wget ca-certificates \
-    &&  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - \
+echo "Installing postgresql..." \
+    && sudo apt-get install wget ca-certificates \
+    && wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - \
+    && sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list' \
+    && sudo apt-get update \
+    && sudo apt-get install postgresql postgresql-contrib \
+    && echo "PostgreSQL installed. Current version is: " \
+    && psql --version
