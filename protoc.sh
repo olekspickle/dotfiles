d45c78872ce36e280855ab380e80d14ab1e50f7b
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    add protoc compiler install

---
+++ protoc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# this installs protoc globally 
+
+sudo apt update
+sudo apt install -y unzip
+PROTOC_VERSION=$(curl -s "https://api.github.com/repos/protocolbuffers/protobuf/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+
+curl -Lo protoc.zip "https://github.com/protocolbuffers/protobuf/releases/latest/download/protoc-${PROTOC_VERSION}-linux-x86_64.zip"
+sudo unzip -q protoc.zip bin/protoc -d /usr/local
+sudo chmod a+x /usr/local/bin/protoc
+protoc --version
+rm -rf protoc.zip
