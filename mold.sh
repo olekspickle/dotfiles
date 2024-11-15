f2d2f92d5b14ca54f0df53c7542a133f38eb0c99
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    updatemold and nvim config

--- mold.sh
+++ mold.sh
@@ -4,16 +4,22 @@ set -e
 
 # this inststalls mold linker and adds its config to global cargo config
 
-toml_config='
+git clone https://github.com/rui314/mold.git
+mkdir mold/build
+cd mold/build
+git checkout v1.5.1
+../install-build-deps.sh
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=c++ ..
+cmake --build . -j $(nproc)
+sudo cmake --install .
+
+cargo_mold=`
 [target.x86_64-unknown-linux-gnu]
 linker = "/usr/bin/clang"
-rustflags = ["-C", "link-arg=--ld-path=/usr/bin/mold"]
-'
+rustflags = ["-C", "link-arg=-fuse-ld=$(which mold)"]
+`
 
-git clone https://github.com/rui314/mold.git
-cd mold
-git checkout v1.2.1
-make -j$(nproc) CXX=clang++
-sudo make install
+cat $cargo_mold >> ~/.cargo/config
 
+cd ../../
 rm -rf mold
--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -6,13 +6,18 @@ local map = vim.api.nvim_set_keymap
 -- command: the command you want the keypresses to execute.
 -- options: an optional Lua table of options to configure (e.g., silent or noremap).
 
+if vim.env.TERM == 'alacritty' then
+  vim.cmd([[autocmd UIEnter * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[>1u") | endif]])
+  vim.cmd([[autocmd UILeave * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[<1u") | endif]])
+end
+
 -- remap the key used to leave insert mode
 map('i', 'jk', '', {})
 
 -- Toggle plugin stuff
 map('n', '<C-b>', [[:NvimTreeToggle<cr>]], {})
 
-map('n', 'l', [[:IndentLinesToggle<cr>]], {})
+map('n', 'L', [[:IndentLinesToggle<cr>]], {})
 map('n', 't', [[:TagbarToggle<cr>]], {})
 map('n', 'ff', [[:Telescope find_files<cr>]], {})
 
@@ -20,8 +25,8 @@ map('n', 'ff', [[:Telescope find_files<cr>]], {})
 -- map('n', 'c-r', [[:FloatermNew --height=0.6 --width=0.4 --wintype=float --name=floaterm1 --position=topleft --autoclose=2 ranger --cmd="cargo check"]], {})
 
 -- Buffer switch
-map('n', 'c-i', [[:bn<cr>]], {})
-map('n', 'c-tab', [[:bn<cr>]], {})
+map('n', '<c-i>', [[:bn<cr>]], {})
+--map('n', 'c-i', [[:bn<cr>]], {})
 --map('n', 'c-s-H', [[:bp<cr>]], {})
 --map('n', 'cs-Right', [[:bn<cr>]], {})
 --map('n', 'cs-Left', [[:bp<cr>]], {})
