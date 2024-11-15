4c3d17ce894f98464665b197ed188381ce28e037
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    add nomad to hashi installation

--- nomad.sh
+++ nomad.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt update && \
-  sudo apt install wget gpg coreutils
+# Nomad uses CNI plugins to configure network namespaces when using the bridge network mode. All Linux Nomad client nodes using network namespaces must have CNI plugins installed.
+# Install the CNI reference plugins
+curl -L -o cni-plugins.tgz "https://github.com/containernetworking/plugins/releases/download/v1.0.0/cni-plugins-linux-$( [ $(uname -m) = aarch64 ] && echo arm64 || echo amd64)"-v1.0.0.tgz && \
+  sudo mkdir -p /opt/cni/bin && \
+  sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
 
-# Add the HashiCorp GPG key.
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# Ensure your Linux operating system distribution has been configured to allow container traffic through the bridge network to be routed via iptables.
+# These tunables can be set as follows
+echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-arptables
+echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-ip6tables
+echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-iptables
 
-# Add the official HashiCorp Linux repository.
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-# Update and install
-sudo apt update
-sudo apt install nomad
