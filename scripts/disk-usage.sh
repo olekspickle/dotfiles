9cb43c38674b5ffd681783a137121f05e5f5bf2c
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:53:09 +0100

    add log truncation

--- scripts/disk-usage.sh
+++ scripts/disk-usage.sh
@@ -8,3 +8,11 @@ th=${2:-"500M"}
 echo "Analyzing disk usage..."
 du -h -d 1 --threshold=$th $path | sort -hr
 
+# truncate all log files
+# In general it's a bad thing to use rm on the log then recreating the filename, 
+# if another process has the file open then you don't get the space back until that process closes it's handle on it
+# and you can damage it's permissions in ways that are not immediately obvious but cause more problems later on.
+truncate /opt/package/logs/*.log --size 0
+
+# truncate one file safely
+> /var/log/mail.log
