ba3f715841d56106f78330236491d5a1c219e589
Oleks Pickle <olekspickle@users.noreply.github.com>
2024-11-15 07:51:46 +0100

    Add Theme

--- zsh.sh
+++ zsh.sh
@@ -17,3 +17,12 @@ $SHELL --version
 
 echo "Install Oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Install powerlevel9k theme..."
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+echo "ZSH_THEME=powerlevel9k/powerlevel9k" >> ~/.zshrc
+
+echo "POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vcs dir rbenv)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(root_indicator background_jobs status load)" >> ~/.zshrc
+
