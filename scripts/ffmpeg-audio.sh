145e074027c7d3cedf5a63b838952260e3bff0d0
olekspickle <22867443+olekspickle@users.noreply.github.com>
2024-11-29 17:35:33 +0100

    add audio+vide merging with offset

--- scripts/ffmpeg-audio.sh
+++ scripts/ffmpeg-audio.sh
@@ -17,3 +17,11 @@ ffmpeg -i stalkers3.mp3 -ss 00:40:01.250 -to 00:40:30.700 -c copy best/beat3.mp3
 
 # concatenate audio
 ffmpeg -i "concat:beat11.mp3|beat2.mp3|beat3.mp3" -acodec copy result2.mp3
+
+# replace audio for a video with an offset
+# set offset for any of the inputs + or - sets it ahead or behind
+# -itsoffset <+-00:00:00.000> -i <Input>
+# map first(0) stream video to first(0) out, and second(1) audio stream to first(0)
+# -map 0:v:0  -map 1:a:0
+# -c:v copy copy video stream
+ffmpeg -i oyaji.mp4 -itsoffset -00:00:00.500 -i oyaji.wav -c:v copy -map 0:v:0 -map 1:a:0 new.mp4
