5f623095d7fa3308a3a22002dd9c1a98d54d84a1
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-01-16 13:20:54 +0100

    add git goodness

---
+++ setup/git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# add git goodness
+
+# beautiful git branch table representation
+git config --global alias.bb !$HOME/Documents/dotfiles/scripts/git-better-branch.sh
+
+# record the resolution for easier rebases
+git config --global rerere.enabled true
+
+# better git branch by default: columned and sorted
+git config --global column.ui auto
+git config --global branch.sort -committerdate
+
+# setup ssh signature for signed commits
+git config --global gpg.format ssh
+git config --global user.signingkey $HOME/.ssh/pickle_ed25519.pub
+
