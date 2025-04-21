1026e97011101cf3538e3dff70d47ec9bde225f4
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-04-21 16:48:41 +0200

    add clipboard for wayland installation; cleanup redundant stuff; migrate alacritty

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# some scripts are dependent on previous,
+# so follow exit in case of any error
+set -e
+
 # New full machine setup
 # ideally - media should already be copied to $HOME/Pictures
 
@@ -7,7 +11,7 @@
 find . -type f -name "*.sh" -exec chmod +x {} \;
 
 echo "Installing basic tools..."
-sudo apt install pkg-config build-essential curl llvm cmake lld coreutils xclip \
+sudo apt install wl-clipboard pkg-config build-essential curl llvm cmake lld coreutils xclip \
     libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev \
     libpcap-dev libasound2-dev libgtk-3-dev \
     fonts-firacode fonts-powerline nmap yamllint\
@@ -39,8 +43,8 @@ echo "Installing media related tools..."
 ./setup/media.sh
 
 # copy configuration and aliases
-rsync -avzh .config/* ~/.config
+rsync -avzh .config ~/
 rsync -avzh .aws ~/
-cat bashrc -p >> ~/.bashrc
-cp .aliases ~/.aliases
+rsync -avzh .ssh ~/
+cp .aliases.sh ~/.aliases.sh
 
