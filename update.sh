15e6b84502a7b16b8cf5b05699a92df068b18641
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-05-14 11:43:21 +0200

    fix wayland resolution

--- update.sh
+++ update.sh
@@ -7,5 +7,8 @@ sudo apt upgrade -y
 # update flatpak packages
 flatpak update -y
 
+# update rust, mostly to sync nightly
+rustup update
+
 # update nvim packages with lazy
 nvim --headless +"lua require('lazy').sync()" +qa
