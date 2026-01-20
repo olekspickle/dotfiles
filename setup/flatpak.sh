469e7aa3c29f8db11e56f32d2ddd9c6b863c88d8
olekspickle <22867443+olekspickle@users.noreply.github.com>
2026-01-20 13:24:36 +0100

    order flatpaks

--- setup/flatpak.sh
+++ setup/flatpak.sh
@@ -2,15 +2,13 @@
 
 set -e
 echo Installing flatpak...
-# install flatpak
 sudo apt install flatpak
-# add flathub repo
 flatpak remote-add -y --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # flatseal to manage flatpak permissions
 flatpak install -y flathub com.github.tchx84.Flatseal
 
-# firefox
+# firefox browser
 # check if there is snap block mounted
 lsblk -fe7 -o+ro | rg firefox
 case $? in
@@ -22,11 +20,10 @@ case $? in
         echo "Command failed: Minor errors."
         ;;
 esac
+
 # purge snap package
 snap disable firefox
 sudo snap remove --purge firefox
-
-# browser
 flatpak install -y flathub org.mozilla.firefox
 
 # vpn
@@ -35,15 +32,6 @@ flatpak install -y flathub com.surfshark.Surfshark
 # libre office
 flatpak install -y flathub org.libreoffice.LibreOffice
 
-# airshipper
-flatpak install -y flathub net.veloren.airshipper
-
-# OBS
-flatpak install -y flathub com.obsproject.Studio
-
-# peek gif recorder
-flatpak install -y flathub com.uploadedlobster.peek
-
 # power management
 sudo apt install tlp
 sudo systemctl enable tlp --now
@@ -51,34 +39,42 @@ flatpak install flathub com.github.d4nj1.tlpui
 
 # telegram
 flatpak install -y flathub org.telegram.desktop
-# matrix client
-flatpak install flathub im.riot.Riot
+
 # discord
-flatpak install -y flathub com.discordapp.Discord
+# flatpak install -y flathub com.discordapp.Discord
 
 # steam
 flatpak install -y flathub com.valvesoftware.Steam
 
-# pixelart editor
-flatpak install -y flathub com.orama_interactive.Pixelorama
-
-# Blender
-flatpak install -y flathub org.blender.Blender
+# airshipper
+flatpak install -y flathub net.veloren.airshipper
 
 # Orion torrent streamer
 flatpak install -y flathub com.ktechpit.orion
 
-# carla audio host for windows vst plugins
-flatpak install -y flathub studio.kx.carla
-
 # recording
 flatpak install -y flathub org.audacityteam.Audacity
 
-# Reaper
-flatpak install -y flathub fm.reaper.Reaper
+# carla audio host for windows vst plugins
+flatpak install -y flathub studio.kx.carla
+
+# mic effects
+flatpak install -y flathub com.github.wwmm.easyeffects
+
+# pixelart editor
+flatpak install -y flathub com.orama_interactive.Pixelorama
 
 # KDE photodshop
 flatpak install -y flathub org.kde.krita
+
+# OBS screen recorder
+flatpak install -y flathub com.obsproject.Studio
+
+# video editor
+flatpak install flathub org.kde.kdenlive
+
+# Blender
+flatpak install -y flathub org.blender.Blender
 
 # 3D slicer for printer
 flatpak install flathub com.ultimaker.cura
@@ -86,14 +82,21 @@ flatpak install flathub com.ultimaker.cura
 # screenshots if spectacle not available
 #flatpak install -y flathub org.flameshot.Flameshot
 
+# peek gif recorder
+flatpak install -y flathub com.uploadedlobster.peek
+
+
+# matrix client
+# flatpak install flathub im.riot.Riot
+
+# Reaper
+# flatpak install -y flathub fm.reaper.Reaper
+
 # Godot engine
 # flatpak install -y flathub org.godotengine.Godot
 
 # DAW
 # flatpak install -y flathub com.bitwig.BitwigStudio
-
-# mic effects
-flatpak install -y flathub com.github.wwmm.easyeffects
 
 # midid virtual hub
 # flatpak install -y flathub xyz.safeworlds.midiconn
