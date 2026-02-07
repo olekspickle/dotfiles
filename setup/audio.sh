836aa061d67a9ae78ac2dd4694ed28e47bbd5edf
olekspickle <22867443+olekspickle@users.noreply.github.com>
2026-02-07 11:29:03 +0100

    add audio dev stuff to build DSP plugins and ardour

---
+++ setup/audio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# a bunch of dev audio stuff
+sudo apt install -y gperf intltool python3 sassc \
+    libavahi-gobject-dev  libbluetooth-dev  liblo-dev  libtag1-dev \
+    libboost-dev  libboost-iostreams-dev  libboost-thread-dev  \
+    libeigen3-dev libgtk-3-dev libgtkmm-3.0-dev \
+    liblilv-dev liblrdf0-dev libsndfile1-dev libfftw3-dev  \
+    libarhive-dev libpulse-dev libjack-dev lv2-dev vamp-plugin-sdk \
+    librubberband-dev libaubio-dev libusb-dev
+
