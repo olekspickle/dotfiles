91cd604cb13378222a91bb5f93f4646e5d383982
olekspickle <22867443+olekspickle@users.noreply.github.com>
2026-02-11 10:36:53 +0100

    simplify node install with rusty fnm

--- setup/node.sh
+++ setup/node.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# https://github.com/nvm-sh/nvm#install--update-script
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+curl -fsSL https://fnm.vercel.app/install | bash
 
-echo "Installing latest supported node version..."
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-nvm install --lts
-
+# then add
+# .node-version with the content of lts release. example:
+# 24
+# and run
+# fnm install
