3879514f0574c3da3ef34e4f0b857b8614dee831
olekspickle <22867443+olekspickle@users.noreply.github.com>
2026-04-26 22:26:20 +0200

    minor update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,34 +1,43 @@
--- disable netrw in favor of nvim-tree
+vim.loader.enable()
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
---[[ init.lua ]]
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--branch=stable", -- latest stable release
-        lazyrepo,
-        lazypath,
-    })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            {"Failed to clone lazy.nvim:\n", "ErrorMsg"},
-            {out, "WarningMsg"},
-            {"\nPress any key to exit..."},
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
+local function normalize_src(src)
+    if src:match("^[a-z]+://") then
+        return src
+    end
+    return "https://github.com/" .. src:gsub("^/", "")
+end
+
+local function setup_specs(specs_ext)
+    local specs = {}
+    local configs = {}
+
+    for _, spec in ipairs(specs_ext) do
+        if spec.dependencies then
+            for _, dep in ipairs(spec.dependencies) do
+                table.insert(specs, { src = normalize_src(dep.src), version = dep.version })
+            end
+        end
+        table.insert(specs, vim.tbl_extend("force", spec, { src = normalize_src(spec.src) }))
+        if spec.config then
+            table.insert(configs, spec.config)
+        end
+    end
+
+    vim.pack.add(specs)
+
+    for _, config in ipairs(configs) do
+        config()
     end
 end
-vim.opt.rtp:prepend(lazypath)
 
+_G.setup_plugins = function(specs)
+    setup_specs(specs)
+end
 
 require("core.options")
 require("core.keymaps")
 require("core.plugins")
 require("core.config_plugins")
-
